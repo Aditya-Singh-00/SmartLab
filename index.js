f88401946a3bba0925e24aad61e8826287772d56
Aditya Singh <deviladi00@gmail.com>
2022-05-21 13:37:19 +0530

    added index.js

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-auth.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyD0TftQQlr--6pSMyUXzw5QT4pFM1kB-HM",
-    authDomain: "smart-home-16c6c.firebaseapp.com",
-    databaseURL: "https://smart-home-16c6c-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "smart-home-16c6c",
-    storageBucket: "smart-home-16c6c.appspot.com",
-    messagingSenderId: "1020150526141",
-    appId: "1:1020150526141:web:d0ef2d2dd41178838447d4"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-auth.onAuthStateChanged((user) => {
+firebase.auth().onAuthStateChanged((user) => {
     if (user) {
         document.getElementById("login").style.display = "none";
         document.getElementById("logged-in").style.display = "block";
@@ -30,7 +14,7 @@ function login() {
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
 
-    signInWithEmailAndPassword(auth, email, password)
+    firebase.auth().signInWithEmailAndPassword(email, password)
         .then((userCredential) => {
 
         })
